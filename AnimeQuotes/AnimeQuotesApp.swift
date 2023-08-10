67007c22a14d4f7de62bd27af236265c7d17b491
Lexi McQueen <alexismcqueen01@gmail.com>
2023-08-09 20:18:56 -0400

    Initial Commit

---
+++ AnimeQuotes/AnimeQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeQuotesApp.swift
+//  AnimeQuotes
+//
+//  Created by Lexi McQueen on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimeQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
