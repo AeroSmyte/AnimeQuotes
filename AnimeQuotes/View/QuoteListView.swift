d5bf7b5deb3804dc6de7c485d4acb58129775c51
Lexi McQueen <alexismcqueen01@gmail.com>
2023-08-16 12:39:13 -0400

    Updating the UI composition, and refactor networking functions from view.

--- AnimeQuotes/View/QuoteListView.swift
+++ AnimeQuotes/View/QuoteListView.swift
@@ -11,55 +11,18 @@ struct QuoteListView: View {
     
     @State private var isPerformingTask = false
     
-    @State private var quote : QuoteModel?
     var body: some View {
+        NavigationView {
             VStack {
-                VStack (spacing: 30) {
-                    Text("Anime Quote")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                    QuoteView()
-                    
-                }
-                .padding()
-                
-                Button {
-                    isPerformingTask = true
-                    Task {
-                        quote = try await getQuote()
-                        isPerformingTask = false
-                    }
-                } label: {
-                    Text("Generate a new quote!")
-                        .padding()
-                    Image(systemName: "hand.thumbsup.fill")
-                        .padding()
-                    
-                }
-                .cornerRadius(12)
+                QuoteView()
+                    .cornerRadius(12)
+                    .shadow(radius: 5)
+
+                Spacer()
             }
-            
-    }
-
-    func getQuote() async throws -> QuoteModel {
-        let endpoint = "https://animechan.xyz/api/random"
-        
-        guard let url = URL(string: endpoint) else {
-            throw QuoteError.invalidURL
+            .padding()
+            .navigationTitle("Anime Quotes")
         }
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-            throw QuoteError.invalidResponse
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            return try decoder.decode(QuoteModel.self, from: data)
-        } catch {
-            throw QuoteError.invalidData
-        }
-
     }
 }
 
--- AnimeQuotes/View/QuoteView.swift
+++ AnimeQuotes/View/QuoteView.swift
@@ -8,30 +8,75 @@
 import SwiftUI
 
 struct QuoteView: View {
+    @State private var isPerformingTask = false
+    
+    @State private var animate = false
+
+    @State private var quote : QuoteModel?
+    
     var body: some View {
         VStack (spacing: 30) {
             HStack {
                 VStack(alignment: .leading, spacing: 8) {
-                    Text("Character Name")
+                    Text(quote?.character ?? "The person who said this...")
                         .font(.title)
                         .bold()
-                    Text("The name of this anime is...")
+                    Text(quote?.anime ?? "The anime it came from...")
                         .font(.subheadline)
                         .foregroundColor(.gray)
 
-                    Text("This is a very long quote. A very long quote indeed.")
+                    Text(quote?.quote ?? "The person who said this...")
                         .font(.title3)
                         .italic()
                         .foregroundColor(.gray)
                 }
                 Spacer()
             }.padding()
+            
+            Button {
+                isPerformingTask.toggle()
+                Task {
+                    quote = try await getQuote()
+                    isPerformingTask.toggle()
+                }
+                
+            } label: {
+                HStack(spacing: 10) {
+                    Text("New Quote!")
+                    Image(systemName: "arrow.clockwise")
+                }
+                .foregroundColor(.white)
+                .font(.headline)
+                .padding(20)
+                .background(Color.indigo)
+                .cornerRadius(10)
+
+            }
+            .padding(.bottom)
         }
         .background(Color.white)
         .cornerRadius(12)
         .shadow(radius: 5)
-        
-        
+    }
+
+    func getQuote() async throws -> QuoteModel {
+        let endpoint = "https://animechan.xyz/api/random"
+
+        guard let url = URL(string: endpoint) else {
+            throw QuoteError.invalidURL
+        }
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw QuoteError.invalidResponse
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(QuoteModel.self, from: data)
+        } catch {
+            throw QuoteError.invalidData
+        }
     }
 }
 
